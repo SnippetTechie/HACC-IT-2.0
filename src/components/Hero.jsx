61bc793f2235a457a390475b845bba14555c14e7
GokulMariyappan <gokulmariyappan1@gmail.com>
2025-09-12 23:42:50 +0530

    draft 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -168,7 +168,7 @@ export default function Hero() {
           transition={{ duration: 0.6, delay: 0.4 }}
           className="organizer"
         >
-          organized by HACATHON AND CODING CLUB – School of C & IT
+          organized by HACATHON AND CODING CLUB
         </motion.p>
       </div>
     </section>
